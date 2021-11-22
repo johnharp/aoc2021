ae9b60f2e9fe1bf5dc67347ffea9b943d3599387
John Harper <john@harper.link>
2021-11-21 20:14:45 -0600

    day01 setup

---
+++ day01/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace day01
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMathStillWorks()
+        {
+            Assert.AreEqual(true, 1 + 1 == 2);
+        }
+    }
+}
