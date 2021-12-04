27fc63d45398b77d7141008c33898e5c28daa393
John Harper <john@harper.link>
2021-12-04 13:14:57 -0600

    scaffold day 5

---
+++ day05/PuzzleInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day05
+{
+    public class PuzzleInput
+    {
+        public string[] Lines;
+
+        public PuzzleInput(string filename)
+        {
+            ReadFile(filename);
+        }
+
+        private void ReadFile(string filename)
+        {
+            // more efficient to leave this as an enumerable
+            // but changing to an array in case we need random
+            // access to the lines as part of the puzzle
+            Lines = File.ReadLines($"../../../{filename}").ToArray();
+        }
+    }
+}
