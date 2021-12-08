d7ed682a294418f4e902184d32f889b4423fb44b
John Harper <john@harper.link>
2021-12-08 17:16:51 -0600

    day 09 scaffold

---
+++ day09/PuzzleInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day09
+{
+    public class PuzzleInput
+    {
+        public string[] Lines;
+
+        public PuzzleInput(string filename)
+        {
+            ReadInputFile($"../../../{filename}");
+        }
+
+        public void ReadInputFile(string filename)
+        {
+            Lines = File.ReadAllLines(filename);
+        }
+    }
+}
