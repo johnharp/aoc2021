67e266c402d10ad67e5ebca3625498523bbbcc33
John Harper <john@harper.link>
2021-12-08 11:55:16 -0600

    working on day 08 part 2

--- day08/Program.cs
+++ day08/Program.cs
@@ -11,7 +11,7 @@ namespace day08
 
         static void Main(string[] args)
         {
-            PuzzleInput input = new PuzzleInput("input-example.txt");
+            PuzzleInput input = new PuzzleInput("input.txt");
 
             Part1(input);
             Part2(input);
@@ -72,14 +72,115 @@ namespace day08
 
             WriteLine(patterns, outputs);
 
+            // "1" is the only number displayed using 2 segments
             string signalsForOne = FindPatternOfLength(2, patterns);
             OnlyTheseSignalsCouldBeTheseSegments(signalsForOne, "cf", facts);
 
+            // "7" is the only number displayed using 3 segments
             string signalsForSeven = FindPatternOfLength(3, patterns);
             OnlyTheseSignalsCouldBeTheseSegments(signalsForSeven, "acf", facts);
 
+            // "4" is the only number displayed using 4 segments
             string signalsForFour = FindPatternOfLength(4, patterns);
             OnlyTheseSignalsCouldBeTheseSegments(signalsForFour, "bcdf", facts);
+
+            // by this point we have already have determined the 2 signals
+            // that could be the "c" and "f" segments.  We just don't know
+            // which is which.
+            //
+            // "0", "6", and "9" are all displayed using 6 segments
+            // The "c" segment is lit in both "0" and "9", but is missing in
+            // "6"
+            // 
+            string[] patternsOfLengthSix = FindPatternsOfLength(6, patterns);
+            if (patternsOfLengthSix.Length != 3) throw new Exception(
+                "There should be three patterns of length six.");
+
+            string[] cfSignals = FindSignalsThatCouldBeOneOfTwoSegments("cf", facts);
+            if (cfSignals.Length != 2) throw new Exception(
+                "There should only be 2 signals that could be the c or f segment");
+            string sig1 = cfSignals[0];
+            string sig2 = cfSignals[1];
+
+            int sig1Count = CountNumTimesSignalInPatterns(sig1, patternsOfLengthSix);
+            int sig2Count = CountNumTimesSignalInPatterns(sig2, patternsOfLengthSix);
+
+            // whichever appears 2 times is the "c"
+            // whichever appears 3 times if the "f"
+
+            if (sig1Count == 2 && sig2Count == 3)
+            {
+                SolveASignal(sig1, "c", facts);
+                SolveASignal(sig2, "f", facts);
+            }
+            else if (sig1Count == 3 && sig2Count == 2)
+            {
+                SolveASignal(sig2, "c", facts);
+                SolveASignal(sig1, "f", facts);
+            }
+            else
+            {
+                throw new Exception("algorithm failed for cf determination");
+            }
+
+            // Similarly in the 6-length group, "g" is in all three of them
+            // but "e" is only in two
+            string[] egSignals = FindSignalsThatCouldBeOneOfTwoSegments("eg", facts);
+            if (cfSignals.Length != 2) throw new Exception(
+                "There should only be 2 signals that could be the e or g segment");
+            sig1 = egSignals[0];
+            sig2 = egSignals[1];
+
+            sig1Count = CountNumTimesSignalInPatterns(sig1, patternsOfLengthSix);
+            sig2Count = CountNumTimesSignalInPatterns(sig2, patternsOfLengthSix);
+
+            // whichever appears 2 times is the "e"
+            // whichever appears 3 times if the "g"
+
+            if (sig1Count == 2 && sig2Count == 3)
+            {
+                SolveASignal(sig1, "e", facts);
+                SolveASignal(sig2, "g", facts);
+            }
+            else if (sig1Count == 3 && sig2Count == 2)
+            {
+                SolveASignal(sig2, "e", facts);
+                SolveASignal(sig1, "g", facts);
+            }
+            else
+            {
+                throw new Exception("algorithm failed for eg determination");
+            }
+
+            // Similarly in the 6-length group, "b" is in all three of them
+            // but "d" is only in two
+            string[] bdSignals = FindSignalsThatCouldBeOneOfTwoSegments("bd", facts);
+            if (bdSignals.Length != 2) throw new Exception(
+                "There should only be 2 signals that could be the b or d segment");
+            sig1 = bdSignals[0];
+            sig2 = bdSignals[1];
+
+            sig1Count = CountNumTimesSignalInPatterns(sig1, patternsOfLengthSix);
+            sig2Count = CountNumTimesSignalInPatterns(sig2, patternsOfLengthSix);
+
+            // whichever appears 2 times is the "d"
+            // whichever appears 3 times if the "b"
+
+            if (sig1Count == 2 && sig2Count == 3)
+            {
+                SolveASignal(sig1, "d", facts);
+                SolveASignal(sig2, "b", facts);
+            }
+            else if (sig1Count == 3 && sig2Count == 2)
+            {
+                SolveASignal(sig2, "d", facts);
+                SolveASignal(sig1, "b", facts);
+            }
+            else
+            {
+                throw new Exception("algorithm failed for bd determination");
+            }
+
 
             //ApplyRuleFor1(patterns, facts);
             //ApplyRuleFor7(patterns, facts);
@@ -177,6 +278,18 @@ namespace day08
             return v;
         }
 
+        private static string[] FindSignalsThatCouldBeOneOfTwoSegments(string pair, Dictionary<string, string> facts)
+        {
+            List<string> keys = new List<string>();
+
+            foreach(string key in ALLSIGNALS)
+            {
+                if (facts[key] == pair) keys.Add(key);
+            }
+
+            return keys.ToArray();
+        }
+
         private static void OnlyTheseSignalsCouldBeTheseSegments(string signals, string segments, Dictionary<string, string> facts)
         {
             foreach (string key in ALLSIGNALS)
@@ -207,6 +320,16 @@ namespace day08
             }
         }
 
+        private static int CountNumTimesSignalInPatterns(string signal, string[] patterns)
+        {
+            int count = 0;
+            foreach(var pattern in patterns)
+            {
+                if (pattern.Contains(signal)) count++;
+            }
+            return count;
+        }
+
         private static void ApplyRuleFor1And7(string[] patterns, Dictionary<string, string> facts)
         {
             // find the patterns of length 2 (this will display "1") and length
@@ -219,12 +342,27 @@ namespace day08
 
         private static string FindPatternOfLength(int l, string[] patterns)
         {
+            string[] matches = FindPatternsOfLength(l, patterns);
+            if (matches.Length == 1)
+            {
+                return matches[0];
+            }
+            else
+            {
+                throw new Exception(
+                    $"Error: there should be exactly one pattern of length {l}\n" +
+                    $"Found {matches.Length} matches instead.  This shouldn't happen");
+            }
+        }
+
+        private static string[] FindPatternsOfLength(int l, string[] patterns)
+        {
+            List<string> matches = new List<string>();
             foreach(var pattern in patterns)
             {
-                if (pattern.Length == l) return pattern;
+                if (pattern.Length == l) matches.Add(pattern);
             }
-
-            throw new Exception($"Error: didn't find a pattern of length {l}.  This shouldn't happen."); ;
+            return matches.ToArray();
         }
 
         private static void SolveASignal(string sig, string seg, Dictionary<string, string> facts)
