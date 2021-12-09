c01f5d6a3d7a80ff491d26fdf33479aeac5c3e56
John Harper <john@harper.link>
2021-12-09 10:45:49 -0600

    scaffold day 10

---
+++ day10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "input-example.txt";
+
+            PuzzleInput input = new PuzzleInput(filename);
+        }
+    }
+}
---
+++ day10/PuzzleInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace day10
+{
+    public class PuzzleInput
+    {
+        public string[] Lines;
+
+        public PuzzleInput(string filename)
+        {
+            ReadInputFile($"../../../{filename}");
+        }
+
+        public void ReadInputFile(string fullpath)
+        {
+            Lines = File.ReadAllLines(fullpath);
+        }
+    }
+}
