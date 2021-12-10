575cc491746d9bff6de3268623872d0546a2de07
John Harper <john@harper.link>
2021-12-10 08:14:05 -0600

    day10 part2 complete

--- day10/Program.cs
+++ day10/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace day10
 {
@@ -10,19 +11,24 @@ namespace day10
             string filename = "input.txt";
 
             PuzzleInput input = new PuzzleInput(filename);
-            Part1(input);
+            Solve(input);
         }
 
-        static void Part1(PuzzleInput input)
+        static void Solve(PuzzleInput input)
         {
             long errorScore = 0;
+            List<long> AutocorrectScores = new List<long>();
 
             foreach (string line in input.Lines)
             {
                 Stack<char> s = new Stack<char>();
+                bool isCorrupted = false;
 
-                foreach(var c in line)
+                char[] characters = line.ToCharArray();
+                for (int i = 0; i< characters.Length && !isCorrupted; i++)
                 {
+                    char c = characters[i];
+
                     if (IsOpeningBracket(c))
                     {
                         s.Push(c);
@@ -36,14 +42,40 @@ namespace day10
                             {
                                 // corrupted chunk
                                 errorScore += ErrorScore(c);
-                                continue;
+                                isCorrupted = true;
+                                break;
                             }
                         }
                     }
                 }
+
+                if (s.Count > 0 && !isCorrupted)
+                {
+                    long totalAutocorrectScore = 0;
+
+                    // Incomplete line
+                    Console.Out.Write("Incomplete: ");
+                    while (s.Count > 0)
+                    {
+                        char c = s.Pop();
+                        totalAutocorrectScore *= 5;
+                        totalAutocorrectScore += AutocorrectScore(c);
+                        Console.Out.Write(c);
+                    }
+                    Console.Out.Write(
+                        $"  autocorrect score: {totalAutocorrectScore}");
+                    AutocorrectScores.Add(totalAutocorrectScore);
+                    Console.Out.WriteLine();
+                }
             }
 
             Console.Out.WriteLine($"Total error score = {errorScore}");
+            AutocorrectScores = AutocorrectScores.OrderBy(x => x).ToList();
+            int totalNumScores = AutocorrectScores.Count();
+            int middleScoreIndex = totalNumScores / 2;
+            Console.Out.WriteLine($"Middle score index = {middleScoreIndex}");
+            long middleScore = AutocorrectScores[middleScoreIndex];
+            Console.Out.WriteLine($"Autocorrect middle score = {middleScore}");
         }
 
         static bool IsOpeningBracket(char c)
@@ -73,6 +105,17 @@ namespace day10
             else if (c == ']') score = 57;
             else if (c == '}') score = 1197;
             else if (c == '>') score = 25137;
+
+            return score;
+        }
+
+        static int AutocorrectScore(char c)
+        {
+            int score = 0;
+            if (c == '(') score = 1;
+            else if (c == '[') score = 2;
+            else if (c == '{') score = 3;
+            else if (c == '<') score = 4;
 
             return score;
         }
