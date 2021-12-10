9d39d2237610d8693eb0f56bb961feee0dda0e17
John Harper <john@harper.link>
2021-12-10 08:37:14 -0600

    day11 scaffolding

---
+++ day11/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "input-example.txt";
+            PuzzleInput input = new PuzzleInput(filename);
+        }
+    }
+}
