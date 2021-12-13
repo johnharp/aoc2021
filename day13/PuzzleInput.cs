3961a35379bb8563daead66cd847b1c20d6980da
John Harper <john@harper.link>
2021-12-12 18:39:17 -0600

    day 13 scaffold

---
+++ day13/PuzzleInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace day13
+{
+    public class PuzzleInput
+    {
+        public string[] Lines;
+
+        public PuzzleInput(string filename)
+        {
+            ReadInputFile($"../../../{filename}");
+        }
+
+        private void ReadInputFile(string fullpath)
+        {
+            Console.WriteLine($"Reading {fullpath}");
+            Lines = File.ReadAllLines(fullpath);
+        }
+    }
+}
