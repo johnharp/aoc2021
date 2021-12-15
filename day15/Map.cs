dd701406bc931066ddaa5f521dfee5e7fc7ca276
John Harper <john@harper.link>
2021-12-15 11:21:33 -0600

    day15 part1

--- day15/Map.cs
+++ day15/Map.cs
@@ -5,6 +5,10 @@ namespace day15
     {
         // Values[col][row]
         public long[][] Values;
+        // LPS = Least Path Sum
+        public long[][] LPS;
+
+
         public long NumCols;
         public long NumRows;
 
@@ -14,9 +18,12 @@ namespace day15
             NumCols = initialValues[0].Length;
 
             Values = new long[NumCols][];
+            LPS = new long[NumCols][];
+
             for (long col=0; col<NumCols; col++)
             {
                 Values[col] = new long[NumRows];
+                LPS[col] = new long[NumRows];
             }
 
             for(long row = 0; row<NumRows; row++)
@@ -30,5 +37,48 @@ namespace day15
                 }
             }
         }
+
+        public void ComputeLeastPathSums()
+        {
+            for (long row = NumRows - 1; row >= 0; row--)
+            {
+                for (long col = NumCols - 1; col >= 0; col--)
+                {
+                    long lps;
+
+                    if (col == NumCols - 1 && row == NumRows - 1)
+                    {
+                        // we're at the goal
+                        lps = 0;
+                    }
+                    else
+                    {
+                        long rightLps = col == NumCols - 1 ? long.MaxValue :
+                            Values[col + 1][row] + LPS[col + 1][row];
+
+                        long downLps = row == NumRows - 1 ? long.MaxValue :
+                            Values[col][row + 1] + LPS[col][row + 1];
+
+                        lps = Math.Min(rightLps, downLps);
+                    }
+
+                    LPS[col][row] = lps;
+                }
+            }
+        }
+
+        public void DumpLeastPathSums()
+        {
+            for (int row = 0; row < NumRows; row++)
+            {
+                for (int col = 0; col < NumCols; col++)
+                {
+                    Console.Write(LPS[col][row].ToString("D2"));
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
     }
 }
--- day15/Program.cs
+++ day15/Program.cs
@@ -6,10 +6,17 @@ namespace day15
     {
         static void Main(string[] args)
         {
-            string filename = "input-example.txt";
+            string filename = "input.txt";
 
             var input = new PuzzleInput(filename);
+            var map = new Map(input.Lines);
+            Part1(map);
+        }
 
+        private static void Part1(Map map)
+        {
+            map.ComputeLeastPathSums();
+            Console.WriteLine($"Least path sum: {map.LPS[0][0]}");
         }
     }
 }
