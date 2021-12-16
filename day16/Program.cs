5d7a9976a640b0e6d4b2c2d7a3aa899532de5abd
John Harper <john@harper.link>
2021-12-15 22:15:08 -0600

    day16 scaffold

---
+++ day16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace day16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "input-example.txt";
+            var input = new PuzzleInput(filename);
+
+
+        }
+    }
+}
