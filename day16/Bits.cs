f5140834f40bad59c5b518b8028f94fa8cf5e453
John Harper <john@harper.link>
2021-12-16 07:06:44 -0600

    HexToDecimal

---
+++ day16/Bits.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace day16
+{
+    public class Bits
+    {
+
+        public string HexToBinary(string hex)
+        {
+            var sb = new StringBuilder();
+            foreach (var hexchar in hex.ToCharArray())
+            {
+                sb.Append(HexDigitToBinary(hexchar));
+            }
+
+            return sb.ToString();
+        }
+
+        public string HexDigitToBinary(char hexchar)
+        {
+            string binary = "";
+            string hexdigit = new string(hexchar, 1);
+            int intValue = Convert.ToInt32(hexdigit, 16);
+
+            binary = Convert.ToString(intValue, 2);
+            binary = binary.PadLeft(4, '0');
+            return binary;
+        }
+
+        public Bits()
+        {
+        }
+    }
+}
---
+++ day16/TestBits.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace day16
+{
+    [TestClass]
+    public class TestBits
+    {
+        [TestMethod]
+        public void HexDigitToBinary()
+        {
+            Bits b = new Bits();
+
+            Assert.AreEqual("0000", b.HexDigitToBinary('0'));
+            Assert.AreEqual("0001", b.HexDigitToBinary('1'));
+            Assert.AreEqual("0010", b.HexDigitToBinary('2'));
+            Assert.AreEqual("0011", b.HexDigitToBinary('3'));
+            Assert.AreEqual("0100", b.HexDigitToBinary('4'));
+            Assert.AreEqual("0101", b.HexDigitToBinary('5'));
+            Assert.AreEqual("0110", b.HexDigitToBinary('6'));
+            Assert.AreEqual("0111", b.HexDigitToBinary('7'));
+            Assert.AreEqual("1000", b.HexDigitToBinary('8'));
+            Assert.AreEqual("1001", b.HexDigitToBinary('9'));
+            Assert.AreEqual("1010", b.HexDigitToBinary('A'));
+            Assert.AreEqual("1011", b.HexDigitToBinary('B'));
+            Assert.AreEqual("1100", b.HexDigitToBinary('C'));
+            Assert.AreEqual("1101", b.HexDigitToBinary('D'));
+            Assert.AreEqual("1110", b.HexDigitToBinary('E'));
+            Assert.AreEqual("1111", b.HexDigitToBinary('F'));
+        }
+
+        [TestMethod]
+        public void HexToBinary()
+        {
+            Bits b = new Bits();
+            Assert.AreEqual(
+                "110100101111111000101000",
+                b.HexToBinary("D2FE28"));
+
+            Assert.AreEqual(
+                "00111000000000000110111101000101001010010001001000000000",
+                b.HexToBinary("38006F45291200"));
+
+            Assert.AreEqual(
+                "11101110000000001101010000001100100000100011000001100000",
+                b.HexToBinary("EE00D40C823060"));
+        }
+    }
+}
