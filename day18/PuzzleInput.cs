cd425d691efd4b6e259f345d8fed9d86e86e9f00
John Harper <john@harper.link>
2021-12-17 19:15:52 -0600

    day 18 scaffolding

---
+++ day18/PuzzleInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace day18
+{
+    public class PuzzleInput
+    {
+        public string[] Lines;
+        public PuzzleInput(string filename)
+        {
+            ReadInputFile($"../../../{filename}");
+        }
+
+        private void ReadInputFile(string fullpath)
+        {
+            Lines = File.ReadAllLines(fullpath);
+        }
+    }
+}
---
+++ day18/TestPuzzleInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace day18
+{
+    [TestClass]
+    public class TestPuzzleInput
+    {
+        [TestMethod]
+        public void TestExampleInput()
+        {
+            var input = new PuzzleInput("input-example.txt");
+            Assert.AreEqual(10, input.Lines.Length);
+        }
+    }
+}
