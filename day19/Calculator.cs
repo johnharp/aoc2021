fdacb418d056660db5df370698586c40133185b3
John Harper <john@harper.link>
2021-12-19 14:32:47 -0600

    tested that orientations match

---
+++ day19/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day19
+{
+    public class Calculator
+    {
+        public List<Orientation> Orientations;
+
+        public Calculator()
+        {
+            Orientations = Orientation.PossibleOrientations();
+        }
+
+        public void LookForMatches(Scanner s1, Scanner s2)
+        {
+            foreach(var o in Orientations)
+            {
+                var points = ApplyOrientation(o, s1.Points);
+            }
+        }
+
+        public List<Vector3> ApplyOrientation(Orientation o, List<Vector3> points)
+        {
+            List<Vector3> newPoints = new List<Vector3>();
+
+            foreach(var point in points)
+            {
+                Vector3 newPoint = o.Rotate(point);
+                newPoints.Add(newPoint);
+            }
+
+            return newPoints;
+        }
+    }
+}
--- day19/Program.cs
+++ day19/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace day19
 {
@@ -9,10 +10,41 @@ namespace day19
             string filename = "input-example.txt";
             // string filename = "input.txt";
             var input = new PuzzleInput(filename);
+            var calc = new Calculator();
 
             var scanners = input.Scanners;
 
-            var orientations = Orientation.PossibleOrientations();
+
+            List<Vector3> points = new List<Vector3>()
+            {
+                new Vector3(-1, -1, 1),
+                new Vector3(-2, -2, 2),
+                new Vector3(-3, -3, 3),
+                new Vector3(-2, -3, 1),
+                new Vector3(5, 6, -4),
+                new Vector3(8, 0, 7)
+            };
+
+            Console.WriteLine("-------------------------");
+
+            foreach (Orientation o in Orientation.PossibleOrientations())
+            {
+                var rotatedPoints = calc.ApplyOrientation(o, points);
+                DumpPoints(rotatedPoints);
+                Console.WriteLine("-------------------------");
+            }
+
+
+        }
+
+        public static void DumpPoints(List<Vector3> points)
+        {
+            foreach (var p in points)
+            {
+                Console.WriteLine(p);
+            }
         }
     }
+
+
 }
