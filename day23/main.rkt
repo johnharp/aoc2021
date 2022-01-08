959619d70e852734d73340de6b917059212ae891
John Harper <john@harper.link>
2022-01-08 09:06:16 -0600

    display-state

--- day23/main.rkt
+++ day23/main.rkt
@@ -1,2 +1,7 @@
 #lang racket
 (require "moves.rkt")
+(require "state.rkt")
+
+
+
+(define initial-state (string->state "...........BACDBCDA"))
--- day23/moves.rkt
+++ day23/moves.rkt
@@ -1,26 +1,26 @@
 #lang racket
-(provide moves-from)
+(provide moves-from is-occupied)
 #|
   0   1   2   3   4   5   6   7   8   9  10
          11      13      15      17
          12      14      16      18
 |#
-
+  
 (define (moves-from-14)
-  '((12 11 2 3 4 13)
-    (11 2 3 4 13)
-    (13)
-    (16 15 6 5 4 13)
-    (15 6 5 4 13)
-    (18 17 8 7 6 5 4 13)
-    (17 8 7 6 5 4 13)
-    (0 1 2 3 4 13)
-    (1 2 3 4 13)
-    (3 4 13)
-    (5 4 13)
-    (7 6 5 4 13)
-    (9 8 7 6 5 4 13)
-    (10 9 8 7 6 5 4 13)))
+  '((14 13 4 3 2 11 12)
+    (14 13 4 3 2 11)
+    (14 13)
+    (14 13 4 5 6 15 16)
+    (14 13 4 5 6 15)
+    (14 13 4 5 6 7 8 17 18)
+    (14 13 4 5 6 7 8 17)
+    (14 13 4 3 2 1 0)
+    (14 13 4 3 2 1)
+    (14 13 4 3)
+    (14 13 4 5)
+    (14 13 4 5 6 7)
+    (14 13 4 5 6 7 8 9)
+    (14 13 4 5 6 7 8 9 10)))
 
 (define (moves-from-13)
   '((12 11 2 3 4)
@@ -200,3 +200,10 @@
     [(12) (moves-from-12)]
     [(13) (moves-from-13)]
     [(14) (moves-from-14)]))
+
+;; #t if state is occupied at index
+;; #f if state is empty at index
+(define (is-occupied state index)
+  (not (equal? (vector-ref state index) ".")))
+  
+  
